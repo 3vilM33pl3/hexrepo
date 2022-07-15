eac54618a66d1c5d141b7fefb768302f3817282a
3vilM33pl3 <ovanac01@mail.bbk.ac.uk>
2022-07-15 08:11:53 +0100

    Adding Unreal Engine build file

---
+++ src/hexcom/unreal/HexWorldLibrary.Build.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using UnrealBuildTool;
+
+public class HexWorldLibrary : ModuleRules
+{
+	public IEnumerable<string> FindFiles(string Dir, string Ext)
+	{
+		return Directory.GetFiles(Dir, "*." + Ext);
+	}
+
+	public HexWorldLibrary(ReadOnlyTargetRules Target) : base(Target)
+	{
+		Type = ModuleRules.ModuleType.External;
+
+
+		if (Target.Platform == UnrealTargetPlatform.Win64)
+		{
+			var libraries = FindFiles(Path.GetFullPath(Path.Combine(ModuleDirectory, "Win64/Release")), "lib");
+			Console.WriteLine(string.Join("Adding library: ", libraries));
+			PublicAdditionalLibraries.AddRange(libraries);
+		}
+
+		if (Target.Platform == UnrealTargetPlatform.Mac)
+		{
+			var libraries = FindFiles(Path.GetFullPath(Path.Combine(ModuleDirectory, "Mac")), "a");
+			Console.WriteLine(string.Join("Adding library: ", libraries));
+			PublicAdditionalLibraries.AddRange(libraries);
+		}
+
+		AddEngineThirdPartyPrivateStaticDependencies(Target, "OpenSSL");
+		AddEngineThirdPartyPrivateStaticDependencies(Target, "zlib");
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] { "CoreUObject", "Engine" });
+
+		PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "include"));
+
+	}
+}
